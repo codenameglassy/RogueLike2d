1360c215e2a3a9fe86117163521cfe2007c730c6
codenameglassy <110374013+ramailogames@users.noreply.github.com>
2026-02-05 13:59:51 +0545

    WEBGL BUILD

--- rougelike2d/Assets/Editor/HierarchySeparator.cs
+++ rougelike2d/Assets/Editor/HierarchySeparator.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class HierarchySeparator : MonoBehaviour
 {
+#if UNITY_EDITOR
     [MenuItem("GameObject/Create Separator", false, 0)]
     private static void CreateSeparator()
     {
         GameObject separator = new GameObject("―――――― Separator ――――――");
-        separator.tag = "EditorOnly"; // optional, so it won’t appear in builds
+        separator.tag = "EditorOnly";
         separator.hideFlags = HideFlags.None;
 
-        // Keep it at root
         if (Selection.activeTransform != null)
         {
             separator.transform.SetParent(Selection.activeTransform.parent);
-            separator.transform.SetSiblingIndex(Selection.activeTransform.GetSiblingIndex() + 1);
+            separator.transform.SetSiblingIndex(
+                Selection.activeTransform.GetSiblingIndex() + 1
+            );
         }
+
         Undo.RegisterCreatedObjectUndo(separator, "Create Separator");
         Selection.activeGameObject = separator;
     }
+#endif
 }
